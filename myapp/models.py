22f52f38d6d86d19ef32022219802762b0c15ac9
Koras02 <wjdgh0727@gmail.com>
2025-03-25 15:55:21 +0900

    모델 정의, 어드민 설정

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) # Title
+    content = models.TextField()   # SubScribe
+    create_at = models.DateTimeField(auto_now_add=True) # create Date  
+    updated_at = models.DateTimeField(auto_now=True) # update Date
+    
+    def __str__(self):
+        return self.title 
