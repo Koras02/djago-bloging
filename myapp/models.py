bb20a90924e4844123603f88d0e627de6582ddb3
Koras02 <wjdgh0727@gmail.com>
2025-04-04 13:55:59 +0900

    뷰 작성- 비즈니스 로직 및 데이터 처리

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Product(models.Model):
+     name = models.CharField(max_length=100) # Product Name 
+     price = models.DecimalField(max_digits=10, decimal_places=2) # Product Price 
+     stock = models.IntegerField() # product stock
+     
+     def __str__(self):
+        return self.name # Product name 
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200) # Title
