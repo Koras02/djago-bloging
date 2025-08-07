db141189e598025ae4482786190038cbe48dfe9e
Koras02 <wjdgh0727@gmail.com>
2025-08-07 09:49:06 +0900

    HTML 템플릿 작성 및 렌더링

--- djangobloging/urls.py
+++ djangobloging/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib import admin
-from django.views.generic import RedirectView
-from myapp.views import product_list
+from django.urls import include
+# from django.views.generic import RedirectView
+# from myapp.views import product_list
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/products/')),
     path('admin/', admin.site.urls),
-    path('products/', product_list, name='product_list')
+    path ('', include('myapp.urls'))
+    # path('', RedirectView.as_view(url='/products/')),
+    # path('products/', product_list, name='product_list'),
 ]
